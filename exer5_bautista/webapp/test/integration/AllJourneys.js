b907e4daa1efc20a9dc3b4ca73f2e8bdab56d6fd
Renz Bautista <renz.l.bautista@accenture.com>
2026-05-16 09:44:21 +0000

    Exercise 5 Creation

--- exer5_bautista/webapp/test/integration/AllJourneys.js
+++ exer5_bautista/webapp/test/integration/AllJourneys.js
@@ -7,7 +7,7 @@ sap.ui.define([
 
 	Opa5.extendConfig({
 		arrangements: new Startup(),
-		viewNamespace: "com.training.exer5bautista.view.",
+		viewNamespace: "com.training.exer5_bautista.view.",
 		autoWait: true
 	});
 });
